42364ce2e62d6641c77b96300d2eee92b7461bfc
Your Name <daniel_l0l@hotmail.com>
2021-12-01 21:37:28 -0300

    feat: .env e algumas alteraçẽs

--- src/livros.controller.ts
+++ src/livros.controller.ts
@@ -13,35 +13,29 @@ import { LivrosService } from './livros.service';
 @Controller('livros')
 export class LivrosController {
   constructor(private livrosService: LivrosService) {}
-  livros: Livro[] = [
-    // new Livro('LIV01', 'Livro TDD e BDD na prática', 29.9),
-    // new Livro('LIV02', 'Livro iniciando com Flutter', 39.9),
-    // new Livro('LIV03', 'Inteligência artificial', 29.9),
-  ];
 
   @Get()
-  obterTodos(): Livro[] {
-    return this.livrosService.obtertodo();
+  async obterTodos(): Promise<Livro[]> {
+    return this.livrosService.obterTodos();
   }
 
   @Get(':id')
-  obterUm(@Param() params): Livro {
+  async obterUm(@Param() params): Promise<Livro> {
     return this.livrosService.obterUm(params.id);
   }
 
   @Post()
-  criar(@Body() livro: Livro) {
-    livro.id = 100;
+  async criar(@Body() livro: Livro) {
     this.livrosService.criar(livro);
   }
 
   @Put()
-  alterar(@Body() livro: Livro): Livro {
+  async alterar(@Body() livro: Livro): Promise<[number, Livro[]]> {
     return this.livrosService.alterar(livro);
   }
 
   @Delete(':id')
-  apagar(@Param() params) {
+  async apagar(@Param() params) {
     this.livrosService.apagar(params.id);
   }
 }
